3edd8b95a7171465bdeb4ee36c0d549dd9531e3d
Lucas Cerqueira Santos <lucascerqueira.art@gmail.com>
2022-07-27 18:37:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,107 +1,123 @@
 using System;
 
 namespace ForInRowGame
-{
-    public class Program
+
+    internal class Program
     {
         static void Main(string[] args)
-
-
         {
 
-            string player1 = "X", player2 = "O";
-
-            int column, row;
-
-            string[,] table = new string[16,16];
-
-            Console.WriteLine("Welcome to the 4 in the row Game Type the name of the players and enjoy!!");
-
-            Console.WriteLine("Type a name of the player 1");
-
-            player1 = Console.ReadLine();
-
-            Console.WriteLine("Type a name of the player 2");
-
-            player2 = Console.ReadLine();
 
 
 
-            while (true)
             {
 
-                Console.Clear();
+                string player1 = "X", player2 = "O", player1Name, player2Name;
 
-                Board(table);
+                int column, row;
 
-                row = Convert.ToInt32(Console.ReadLine());
-                
-                Console.WriteLine(player1 + " Choose a column");
+                string[,] table = new string[16, 16];
 
-                column = Convert.ToInt32(Console.ReadLine());
-                                
-                player1 = table[row,column];
-                
-                Console.WriteLine(player2 + " Choose a column");
+                Console.WriteLine("Welcome to the 4 in the row Game Type the name of the players and enjoy!!");
 
-                column = Convert.ToInt32(Console.ReadLine());
-                
-                player2 = table[row,column];
+                Console.WriteLine("Type a name of the player 1");
 
-                
+                player1Name = Console.ReadLine();
+
+                Console.WriteLine("Type a name of the player 2");
+
+                player2Name = Console.ReadLine();
+
+
 
                 
-
-            }
-
-
-
-            static void Board(string[,] table1)
-
-            {
-                Console.WriteLine("  |01 ||02 ||03 ||04 ||05 ||06 ||07 ||08 ||09 ||10 ||11 ||12 ||13 ||14 ||15 ||16 |");
-                for (int i = 1; i < table1.GetLength(0) + 1; i++)
+                
+                while (true)
                 {
-                    if (i < 10)
+
+                    Console.Clear();
+
+                    Board(table);
+
+                    row = 16;
+
+                    Console.WriteLine(player1Name + " Choose a column");
+
+                    column = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(player1Name + " input a " + player1 + " in a column " + column);
+
+                    
+                    Console.WriteLine(player2Name + " Choose a column");
+
+                    column = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(player1Name + " input a " + player2 + " in a column " + column);
+
+                    
+
+
+                    if (table[row, column] == " " && table[row,column] == player1)
                     {
-                        Console.Write(i + " ");
+                        table[row, column] = player1;
                     }
-                    else
-                    {
-                        Console.Write(i);
-                    }
-
-
-                    for (int j = 0; j < table1.GetLength(1); j++)
-
-                    {
-
-                        Console.Write("|___|");
-
-                    }
-
-
-                    Console.WriteLine();
-
 
                 }
+
+                static void Board(string[,] table)
+                {
+                    Console.WriteLine("  |01 ||02 ||03 ||04 ||05 ||06 ||07 ||08 ||09 ||10 ||11 ||12 ||13 ||14 ||15 ||16 |");
+                    for (int i = 1; i < table.GetLength(0) + 1; i++)
+                    {
+                        if (i < 10)
+                        {
+                            Console.Write(i + " ");
+                        }
+                        else
+                        {
+                            Console.Write(i);
+                        }
+
+
+                        for (int j = 0; j < table.GetLength(1); j++)
+
+                        {
+
+                            Console.Write("|___|");
+
+                        }
+
+
+                        Console.WriteLine();
+
+
+                        //    for (int i = 0; i <= table1.GetLength(0); i++)
+                        //    {
+
+
+                        //        table1[i, column] == " ";
+                        //}
+
+
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             }
-
-
-
-
-
-
-           
-
-
-
-
-
 
 
         }
-
-
     }
 }
