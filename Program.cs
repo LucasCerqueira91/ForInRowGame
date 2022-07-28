1149e03f77d14ac906b6a7f9053b4a85de508872
Lucas Cerqueira Santos <lucascerqueira.art@gmail.com>
2022-07-28 15:51:52 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 
 namespace ForInRowGame
+{
 
     internal class Program
     {
@@ -12,9 +13,9 @@ namespace ForInRowGame
 
             {
 
-                string player1 = "X", player2 = "O", player1Name, player2Name;
+                string player1 = "X", player2 = "O", player1Name, player2Name, turnPlayer;
 
-                int column, row;
+                int column, row, moves = 0;
 
                 string[,] table = new string[16, 16];
 
@@ -30,8 +31,7 @@ namespace ForInRowGame
 
 
 
-                
-                
+
                 while (true)
                 {
 
@@ -47,61 +47,158 @@ namespace ForInRowGame
 
                     Console.WriteLine(player1Name + " input a " + player1 + " in a column " + column);
 
-                    
+
                     Console.WriteLine(player2Name + " Choose a column");
 
                     column = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine(player1Name + " input a " + player2 + " in a column " + column);
+                    Console.WriteLine(player2Name + " input a " + player2 + " in a column " + column);
 
-                    
+                    turnPlayer = player1;
 
 
-                    if (table[row, column] == " " && table[row,column] == player1)
+                    table[row, column] = turnPlayer;
+
+                    if (turnPlayer == "X")
+
                     {
-                        table[row, column] = player1;
+
+                        turnPlayer = player2;
+
+                    }
+
+                    else
+
+                    {
+
+                        turnPlayer = "X";
+
+                    }
+
+                    if (VictoryChecker(table))
+
+                    {
+
+                        Console.WriteLine("The player " + turnPlayer + " is the Winner!!");
+
+
+
+                        break;
+
+                    }
+
+
+
+                    moves = moves + 1;
+
+
+
+                    if (moves == 9)
+
+                    {
+
+                        Console.WriteLine("Draw");
+
+                        break;
+
                     }
 
                 }
 
-                static void Board(string[,] table)
+                static bool VictoryChecker(string[,] table)
+
                 {
-                    Console.WriteLine("  |01 ||02 ||03 ||04 ||05 ||06 ||07 ||08 ||09 ||10 ||11 ||12 ||13 ||14 ||15 ||16 |");
-                    for (int i = 1; i < table.GetLength(0) + 1; i++)
+
+                    for (int i = 0; i < table.GetLength(0); i++)
                     {
-                        if (i < 10)
-                        {
-                            Console.Write(i + " ");
-                        }
-                        else
-                        {
-                            Console.Write(i);
-                        }
-
-
-                        for (int j = 0; j < table.GetLength(1); j++)
+                        if (table[i, 0] == table[i, 1] && table[i, 1] == table[i, 2] && table[i, 1] != " ")
 
                         {
-
-                            Console.Write("|___|");
-
+                            return true;
                         }
+                    }
 
+                    for (int i = 0; i < table.GetLength(1); i++)
+                    {
+                        if (table[0, i] == table[1, i] && table[1, i] == table[2, i] && table[1, i] != " ")
+                        {
+                            return true;
+                        }
+                    }
 
-                        Console.WriteLine();
-
-
-                        //    for (int i = 0; i <= table1.GetLength(0); i++)
-                        //    {
-
-
-                        //        table1[i, column] == " ";
-                        //}
-
+                    for (int i = 0; i < table.GetLength(1); i++)
+                    {
+                        if (table[0, 0] == table[1, 1] && table[1, 1] == table[2, 2] && table[1, i] != " ")
+                        {
+                            return true;
+                        }
 
                     }
+                    for (int i = 0; i < table.GetLength(1); i++)
+                        if (table[0, 2] == table[1, 1] && table[1, 1] == table[2, 0] && table[1, i] != " ")
+                        {
+                            return true;
+                        }
+                    return false;
                 }
 
+
+
+
+            }
+
+            static void Board(string[,] table)
+
+
+            {
+                Console.WriteLine("  |01 ||02 ||03 ||04 ||05 ||06 ||07 ||08 ||09 ||10 ||11 ||12 ||13 ||14 ||15 ||16 |");
+                for (int i = 1; i < table.GetLength(0) + 1; i++)
+                {
+                    if (i < 10)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    else
+                    {
+                        Console.Write(i);
+                    }
+
+
+                    for (int j = 0; j < table.GetLength(1); j++)
+
+                    {
+
+                        Console.Write("|___|");
+
+                    }
+
+
+                    Console.WriteLine();
+
+
+
+
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
 
 
 
